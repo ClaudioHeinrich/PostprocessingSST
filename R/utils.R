09ad3c3998cf0b4483c6badfedc1fdd6a209ecd3
Alex Lenkoski <alex@nr.no>
2017-09-05 19:23:32 +0200

    Plots of the grid

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,11 @@ invert_ym = function(YM)
   ## TODO: verify
   return(c(YM %% 12, YM - YM %% 12 ))
 }
+
+
+restrict_coord = function(lons,lats)
+{
+  ss = parse(text = paste0("(Lon >",lons[1],") & (Lon <",lons[2],")",
+                           "& (Lat >", lats[1],") & (Lat <",lats[2],")"))
+  return(ss)
+}
--- scripts/devel.rectification.R
+++ scripts/devel.rectification.R
@@ -4,6 +4,7 @@ rm(list = ls())
 library(SeasonalForecasting)
 setwd("~/NR/SFE/")
 options(max.print = 1e3)
+print_figs = FALSE
 ##------------------------
 
 ##----- Load Constituents ---
@@ -14,15 +15,51 @@ dt_obs = load_observations(y,m)
 ##--------------------------
 
 
-##-------- Now Organize the Ensemble ----
+##--- Now Organize the Ensemble ----
 dt_ens_coord = unique(dt_ens[,.(Lon,Lat)])
 setkey(dt_ens_coord, "Lat","Lon")
-dt_ens_coord[,Lon_ID := f_lon(Lon)]
-dt_ens_coord[,Lat_ID := f_lat(Lat)]
-dt_ens_coord[, grid:=Lon_ID + Lat_ID]
 
-A = dt_ens_coord[(Lon > 48) & (Lon < 53) & (Lat > 48) & (Lat < 53)]
-B = dt_obs_coord[(Lon > 48) & (Lon < 53) & (Lat > 48) & (Lat < 53)]
+dt_obs_coord = unique(dt_obs[,.(Lon,Lat)])
+setkey(dt_obs_coord, "Lat","Lon")
+##-----------------------------------
 
-plot(A, pch = 19)
-points(B, pch = 19, col="blue")
+##----- Maps of grids ---------
+if(print_figs){pdf("./figures/grid_ensemble.pdf")}else{X11()}
+plot(dt_ens_coord[,.(Lon,Lat)], pch = ".", col="grey50", xlab = "Longitude", ylab = "Latitude", main = "Ensemble")
+map("world", add = TRUE)
+if(print_figs)dev.off()
+
+if(print_figs){pdf("./figures/grid_obs.pdf")}else{X11()}
+plot(dt_obs_coord[,.(Lon,Lat)], pch = ".", col="grey50", xlab = "Longitude", ylab = "Latitude", main = "Observations")
+map("world", add = TRUE)
+if(print_figs)dev.off()
+##------------------------------
+
+
+##------ Let's restrict a bit ---
+lons = c(-10,0)
+lats = c(40,50)
+ss = restrict_coord(lons,lats)
+dt_obs_sub= dt_obs_coord[ eval(ss) ]
+dt_ens_sub =dt_ens_coord[ eval(ss) ]
+##--------------------------------
+
+##------ Get all boxes ---------
+lon_all = sort(dt_obs_sub[,unique(Lon)])
+cutoff_lon = head(lon_all,-1) + diff(lon_all)/2
+n_lon = length(cutoff_lon)
+
+lat_all = sort(dt_obs_sub[,unique(Lat)])
+cutoff_lat = head(lat_all,-1) + diff(lat_all)/2
+n_lat = length(cutoff_lat)
+##------------------------------
+
+##----- Maps of grids ---------
+if(print_figs){pdf("./figures/grid_sub_ensemble.pdf")}else{X11()}
+plot(dt_ens_sub, pch = 19, col="black", xlab = "Longitude", ylab = "Latitude")
+points(dt_obs_sub,pch=19, col="blue")
+map("world", add = TRUE)
+for(i in 1:length(cutoff_lon))abline(v = cutoff_lon[i], lty =2 , col="blue")
+for(i in 1:length(cutoff_lon))abline(h = cutoff_lat[i], lty =2 , col="blue")
+if(print_figs)dev.off()
+##------------------------------
