bd44b7ca88e63d4c6e5efca9354ca7d94dceb701
Alex Lenkoski <alex@nr.no>
2017-11-17 14:34:28 +0100

    Cleaning up scripts folder

--- scripts/devel.quick.rank.R
+++
@@ -1,6 +0,0 @@
-##------- Cpp function equivalent ------
-## First read the source code from a separate file
-src <- paste(readLines("./example2.cpp"), collapse="\n")
-## Now hand the source to cppFunction for compilation
-cppFunction(src)
-##-----------------------------------------
--- scripts/devel_make_reduced_data.R
+++
@@ -1,4 +0,0 @@
-library(SeasonalForecasting)
-
-
-obs_unc_cov(load_reduced_data = FALSE)
