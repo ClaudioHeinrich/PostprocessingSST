6724686cf64cf2a6108891eb7ad6366f6c4606e5
Claudio Heinrich <claudio@samba8.ad.nr.no>
2018-02-21 13:33:47 +0100

    whatever

--- scripts/mean.score.plot.R
+++ scripts/mean.score.plot.R
@@ -13,7 +13,7 @@ test_bias_correct = function(model = "NorESM",
                              save.dir = "~/PostClimDataNoBackup/SFE/Derived/"
 ){
   dt = load_combined_wide(model = model)
-  setnames(dt,"senorge_grid_id","grid_id")
+  if(model == "senorge")setnames(dt,"senorge_grid_id","grid_id")
   
   file.out = paste0(save.dir,"glob.scores.bc.",method,".",model,".RData")
   
@@ -55,20 +55,20 @@ test_bias_correct = function(model = "NorESM",
 
 #----- these lines take their time -----
 
-#test_bias_correct(method = "gwa")
-#test_bias_correct(method = "ema")
-test_bias_correct(model = "senorge", method = "gwa")
-test_bias_correct(model = "senorge", method = "ema")
+test_bias_correct(method = "gwa")
+test_bias_correct(method = "ema")
+#test_bias_correct(model = "senorge", method = "gwa")
+#test_bias_correct(model = "senorge", method = "ema")
 
 
 #----- plot mean scores for gwa -------
 
-#load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.gwa.RData")
-#plot.dir = "./figures/"
+load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.gwa.RData")
+plot.dir = "./figures/"
 
 # for senorge:
-load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.gwa.senorge.RData")
-plot.dir = "./figures/senorge/"
+#load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.gwa.senorge.RData")
+#plot.dir = "./figures/senorge/"
 
 
 mean_scores = sc[,.(RMSE_bar = mean(RMSE,na.rm = TRUE), MAE_bar = mean(MAE,na.rm = TRUE)), by = win_length]
@@ -111,6 +111,8 @@ points(x = mean_scores[,win_length][min_loc_MAE],
 
 legend("topright",legend = c("RMSE","MAE"),lty = c(1,1),col = c("blue","darkgreen"))
 dev.off()
+
+#----------------------
 
 #--------plot mean scores for ema --------
 
@@ -163,6 +165,76 @@ points(x = mean_scores[,ratio][min_loc_MAE],
 
 legend("right",legend = c("RMSE","MAE"),lty = c(1,1),col = c("blue","darkgreen"))
 dev.off()
+
+#---------------------------------------------
+
+
+#--------- get RMSE for both bias correction methods into plots with same y-range
+
+load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.gwa.RData")
+sc_sma = sc[YM>2001*12, .( MRMSE = mean(RMSE)), by = win_length]
+load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.ema.RData")
+sc_ema = sc[YM>2001*12, .( MRMSE = mean(RMSE)), by = ratio]
+
+plot.dir = "./figures/"
+
+y_range = range(c(sc_sma[,MRMSE],sc_ema[,MRMSE]))  
+
+pdf(paste0(plot.dir,"mean_scores_sma.pdf"))
+plot(x = sc_sma[,win_length],
+     y = sc_sma[,MRMSE],
+     ylim = y_range,
+     type = "b",
+     col = "blue",
+     main = "RMSE for bias correction by SMA",
+     xlab = "window length",
+     ylab = "mean RMSE"
+)
+
+#---- add minimum -----
+abline(h = sc_sma[,min(MRMSE)], lty = "dashed", col = adjustcolor("blue",alpha = .5))
+
+min_loc_RMSE = sc_sma[,which.min(MRMSE)]
+
+points(x = sc_sma[,win_length][min_loc_RMSE],
+       y = sc_sma[,MRMSE][min_loc_RMSE],
+       col = "blue",
+       bg = "blue",
+       pch = 21)
+dev.off()
+
+#plot for ema
+
+pdf(paste0(plot.dir,"mean_scores_ema.pdf"))
+plot(x = sc_ema[,ratio],
+     y = sc_ema[,MRMSE],
+     ylim = y_range,
+     type = "b",
+     col = "darkgreen",
+     main = "RMSE for bias correction by EMA",
+     xlab = "ratio",
+     ylab = "mean RMSE"
+)
+
+#---- add minimum -----
+abline(h = sc_ema[,min(MRMSE)], lty = "dashed", col = adjustcolor("darkgreen",alpha = .5))
+
+min_loc_RMSE = sc_ema[,which.min(MRMSE)]
+
+points(x = sc_ema[,ratio][min_loc_RMSE],
+       y = sc_ema[,MRMSE][min_loc_RMSE],
+       col = "darkgreen",
+       bg = "darkgreen",
+       pch = 21)
+
+dev.off()
+
+
+#----------------------
+
+
+
+
 
 
 #--------------------------------------------------------------------------
@@ -266,7 +338,10 @@ dt[,"oos_clim_past" := (cumsum(SST_bar) - SST_bar)/(year - min(year) ),
 
 save(dt, file = "~/PostClimDataNoBackup/SFE/Derived/dt_combine_wide_bias.RData")
 
-glob_mean_sc = dt[,.( "RMSE" = sqrt(mean( (SST_bar - SST_hat)^2, na.rm=TRUE)),
+
+
+glob_mean_sc = dt[,.( month,"RMSE_raw" = sqrt(mean( (SST_bar - Ens_bar)^2, na.rm=TRUE)),
+                              "RMSE_bc" = sqrt(mean( (SST_bar - SST_hat)^2, na.rm=TRUE)),
                               "MAE" = mean(abs(SST_bar - SST_hat), na.rm=TRUE),
                               "RMSE_clim" = sqrt(mean( (SST_bar - oos_clim)^2, na.rm=TRUE)),
                               "MAE_clim" = mean(abs(SST_bar - oos_clim), na.rm=TRUE),
@@ -274,10 +349,37 @@ glob_mean_sc = dt[,.( "RMSE" = sqrt(mean( (SST_bar - SST_hat)^2, na.rm=TRUE)),
                               "MAE_clim_past" = mean(abs(SST_bar - oos_clim_past), na.rm=TRUE)),
                           keyby = YM]
 
-reduced_mean_sc = glob_mean_sc[,.("RMSE_clim" = mean(RMSE_clim),
-                                  "MAE_clim" = mean(MAE_clim),
-                                  "RMSE_clim_past" = mean(RMSE_clim_past),
-                                  "MAE_clim_past" = mean(MAE_clim_past)),]
+reduced_mean_sc = glob_mean_sc[,.("RMSE_raw" = mean(RMSE_raw), 
+                                  "RMSE_bc" = mean(RMSE_bc), 
+                                  "RMSE_clim" = mean(RMSE_clim)),by = month]
+
+
+rr = range(reduced_mean_sc[,.(RMSE_raw,RMSE_bc,RMSE_clim)])
+
+plot.dir = "./figures/"
+pdf(paste0(plot.dir,"RMSE_by_month_3.pdf"))
+
+plot(x = reduced_mean_sc[,month],
+     y = reduced_mean_sc[,RMSE_raw],
+     col = "blue",
+     type = "b",
+     ylim = rr,
+     main = "RMSE by month",
+     ylab = "RMSE",
+     xlab = "month")
+lines(x = reduced_mean_sc[,month],
+      y = reduced_mean_sc[,RMSE_bc],
+      col = "darkred",
+      type = "b")
+lines(x = reduced_mean_sc[,month],
+      y = reduced_mean_sc[,RMSE_clim],
+      col = "darkgreen",
+      type = "b")
+legend("topleft", 
+       legend = c("raw forecast","climatology","bias corrected forecast"), 
+       col = c("blue","darkgreen","darkred"),
+       lty = c(1,1,1))
+dev.off()
 
 
 load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.ema.RData")
