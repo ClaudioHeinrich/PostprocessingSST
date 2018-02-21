effd770264b3d11ccc4a0d593e268c7f7c744114
Claudio Heinrich <claudio@samba8.ad.nr.no>
2018-02-21 13:58:43 +0100

    getting up to date

--- replication/example.residual.plots.R
+++ replication/example.residual.plots.R
@@ -11,14 +11,18 @@ Mvec = 9
 #--- get maximum range for uniform plotting scale ----
 
 rr = c()
+rr_sd = c()
+
 for(Y in Yvec){
   for(M in Mvec){
     rr = c(rr,range(dt[year == Y & month == M,res],na.rm = TRUE))
+    rr_sd = c(rr_sd,range(dt[year == Y & month == M,Ens_sd],na.rm = TRUE))
   }
 }
   
 
 rr = range(rr)
+rr_sd = range(rr_sd)
 
 
 for(Y in Yvec){
@@ -27,3 +31,13 @@ for(Y in Yvec){
     plot_system(Y = Y, M=M, type = "res", print_figs = FALSE, plot_title = paste0("Residual 0",M," / ",Y),rr = rr )
   }
 }
+
+
+
+
+for(Y in Yvec){
+  for(M in Mvec){
+    plot_diagnostic(dt[year == Y & month == M,.(Lon,Lat,Ens_sd)], mn = paste0("Ensemble spread 0",M," / ",Y),rr = rr_sd, save.pdf = TRUE, file.name = paste0("Ens_spread_",M,"_",Y) )
+  }
+}
+
