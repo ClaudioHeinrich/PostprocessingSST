92ec2cebbe8456979c036c7e286351cdcbdd8098
Claudio Heinrich <claudio@samba8.ad.nr.no>
2018-02-23 11:40:38 +0100

    pre-switching-tocentral-git-rep-update

--- scripts/mean.score.plot.R
+++ scripts/mean.score.plot.R
@@ -58,106 +58,8 @@ test_bias_correct = function(method = "gwa", # also accepts ema
 #test_bias_correct(method = "gwa")
 #test_bias_correct(method = "ema")
 
-#----- plot mean scores for gwa -------
 
-load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.gwa.RData")
-plot.dir = "./figures/"
-
-
-mean_scores = sc[,.(RMSE_bar = mean(RMSE,na.rm = TRUE), MAE_bar = mean(MAE,na.rm = TRUE)), by = win_length]
-
-yrange = c(mean_scores[,min(range(RMSE_bar),range(MAE_bar))],
-           mean_scores[,max(range(RMSE_bar),range(MAE_bar))])
-
-pdf(paste0(plot.dir,"mean_scores_gwa.pdf"))
-plot(x = mean_scores[,win_length],
-     y = mean_scores[,RMSE_bar],
-     ylim = yrange,
-     type = "b",
-     col = "blue",
-     main = "mean scores for SMA by length of window",
-     xlab = "window length",
-     ylab = "mean score"
-)
-lines( mean_scores[,win_length], 
-       mean_scores[,MAE_bar],
-       type = "b",
-       col = "darkgreen")
-
-#---- add minima -----
-abline(h = mean_scores[,min(RMSE_bar)], lty = "dashed", col = adjustcolor("blue",alpha = .5))
-abline(h = mean_scores[,min(MAE_bar)], lty = "dashed", col = adjustcolor("darkgreen",alpha = .5))
-
-min_loc_RMSE = mean_scores[,which.min(RMSE_bar)]
-min_loc_MAE = mean_scores[,which.min(MAE_bar)]
-
-points(x = mean_scores[,win_length][min_loc_RMSE],
-       y = mean_scores[,RMSE_bar][min_loc_RMSE],
-       col = "blue",
-       bg = "blue",
-       pch = 21)
-points(x = mean_scores[,win_length][min_loc_MAE],
-       y = mean_scores[,MAE_bar][min_loc_MAE],
-       col = "darkgreen",
-       bg = "darkgreen",
-       pch = 21)
-
-legend("topright",legend = c("RMSE","MAE"),lty = c(1,1),col = c("blue","darkgreen"))
-dev.off()
-
-#----------------------
-
-#--------plot mean scores for ema --------
-
-load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.ema.RData")
-plot.dir = "./figures/"
-
-mean_scores = sc[,.(RMSE_bar = mean(RMSE,na.rm = TRUE), MAE_bar = mean(MAE, na.rm = TRUE)), by = ratio]
-
-yrange = c(mean_scores[,min(range(RMSE_bar),range(MAE_bar))],
-           mean_scores[,max(range(RMSE_bar),range(MAE_bar))])
-
-
-pdf(paste0(plot.dir,"mean_scores_ema.pdf"))
-plot(x = mean_scores[,ratio],
-     y = mean_scores[,RMSE_bar],
-     ylim = yrange,
-     type = "b",
-     col = "blue",
-     main = "Mean scores for EMAs by ratio",
-     xlab = "ratio",
-     ylab = "mean score"
-)
-lines( mean_scores[,ratio], 
-       mean_scores[,MAE_bar],
-       type = "b",
-       col = "darkgreen")
-
-#---- add minima -----
-abline(h = mean_scores[,min(RMSE_bar)], lty = "dashed", col = adjustcolor("blue",alpha = .5))
-abline(h = mean_scores[,min(MAE_bar)], lty = "dashed", col = adjustcolor("darkgreen",alpha = .5))
-
-min_loc_RMSE = mean_scores[,which.min(RMSE_bar)]
-min_loc_MAE = mean_scores[,which.min(MAE_bar)]
-
-points(x = mean_scores[,ratio][min_loc_RMSE],
-       y = mean_scores[,RMSE_bar][min_loc_RMSE],
-       col = "blue",
-       bg = "blue",
-       pch = 21)
-points(x = mean_scores[,ratio][min_loc_MAE],
-       y = mean_scores[,MAE_bar][min_loc_MAE],
-       col = "darkgreen",
-       bg = "darkgreen",
-       pch = 21)
-
-legend("right",legend = c("RMSE","MAE"),lty = c(1,1),col = c("blue","darkgreen"))
-dev.off()
-
-#---------------------------------------------
-
-
-#--------- get RMSE for both bias correction methods into plots with same y-range
+#--------- plot RMSE for bias correction with SMA and EMA 
 
 load("~/PostClimDataNoBackup/SFE/Derived/glob.scores.bc.gwa.RData")
 sc_sma = sc[YM>2001*12, .( MRMSE = mean(RMSE)), by = win_length]
@@ -166,6 +68,7 @@ sc_ema = sc[YM>2001*12, .( MRMSE = mean(RMSE)), by = ratio]
 
 plot.dir = "./figures/"
 
+# ensure that they are plotted vs the same range
 y_range = range(c(sc_sma[,MRMSE],sc_ema[,MRMSE]))  
 
 pdf(paste0(plot.dir,"mean_scores_sma.pdf"))
