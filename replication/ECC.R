b822b78147a6dfa506cb3719604ee91b94a9f3ed
Claudio Heinrich <claudio@nr.no>
2018-07-20 15:18:41 +0200

    Implementation of ECC-Q

--- replication/ECC.R
+++ replication/ECC.R
@@ -8,8 +8,10 @@
 #' 
 #' @description Generates forecasts post-processed by ensemble copula coupling (ECC)
 #' 
-#' @param dt the data table containing bias and variance estimates, if NULL, it is loaded.
+#' @param dt the data table containing bias and variance estimates.
+#' @param y,m vectors contining the years and months for the forecast, default are all years and months in dt.
 #' @param ens_size Integer. Size of the NWP ensemble.
+#' @param method takes "R" or "Q". Method of ECC, see Schefzik et al. 2013.
 #' @param truncate logical, whether the forecasted temperature is truncated at freezing temperature.
 #' @param saveorgo Logical, whether we save or not. 
 #' @param save_dir,file_name The directory to save in and the name of the saved file.
@@ -27,40 +29,71 @@
 
 
 
-forecast_ECC = function(dt = NULL,
+forecast_ECC = function(dt,
+                        y = NULL,
+                        m = NULL,
                         ens_size = 9,
+                        method = "Q",
                         saveorgo = TRUE,
                         save_dir = "./Data/PostClim/SFE/Derived/ECC/",
-                        file_name = "ECC_fc.RData"
-                        )
+                        file_name = "ECC_fc.RData")
 {
-  if(is.null(dt))
+  # prepare data table:
+  
+  if(!is.null(y))
   {
-    load_combined_wide(var = TRUE)
+    dt = dt[year %in% y,]
   }
-    
-  # generate noise and marginally post-process each ensemble member
-    
-  na_rows = dt[ ,which(is.na(Bias_Est) |  is.na(Ens_bar) | is.na(var_bar) )]
   
-  length_norm = dt[ -na_rows,.N]
   
-  for(i in 1:ens_size)
+  if(!is.null(m))
+  {
+    dt = dt[month %in% m,]
+  }  
+  
+  na_rows = dt[ ,which(is.na(Bias_Est) |  is.na(Ens_bar) | is.na(SD_hat) )]
+  
+  
+  
+  if(!identical(na_rows,integer(0)))
+  {
+    dt_temp = dt[-na_rows,]
+  } else {
+    dt_temp = dt
+  }
+  
+  # do univariate postprocessing:  
+  
+  length_norm = dt_temp[,.N]
+  
+  if(method == "R")
+  {
+    for(i in 1:ens_size)
     {
-    norm_rv = rnorm(n = length_norm, 
-                    mean = dt[-na_rows,eval(parse(text = paste0("Ens",i))) + Bias_Est], 
-                    sd = dt[-na_rows,SD_hat])
-    dt[ -na_rows,paste0("fc",i) := norm_rv]
+      dt_temp[ ,paste0("fc",i) := rnorm(n = length_norm, 
+                                        mean = dt_temp[,SST_hat], 
+                                        sd = dt_temp[,SD_hat])]
     }
+  }
+  
+  if(method == "Q")
+  {
+    for(i in 1:ens_size)
+    {
+      dt_temp[ ,paste0("fc",i) := qnorm(i/(ens_size + 1), 
+                                        mean = dt_temp[,SST_hat], 
+                                        sd = dt_temp[,SD_hat])]
+    }
+  }
   
   
   # get rank order of the ensemble and reorder the post-processed ensemble to mach the rank order statistic of the ensemble
   
-  rks_ens = dt[,matrixStats::rowRanks(as.matrix(.SD)),.SDcols = paste0("Ens",1:9)]
+  rks_ens = dt_temp[,matrixStats::rowRanks(as.matrix(.SD)),.SDcols = paste0("Ens",1:9)]
   
-  rks_fc = dt[,matrixStats::rowRanks(as.matrix(.SD)),.SDcols = paste0("fc",1:9)]
+  rks_fc = dt_temp[,matrixStats::rowRanks(as.matrix(.SD)),.SDcols = paste0("fc",1:9)]
   
-  fcs = as.matrix(dt[,.SD,.SDcols = paste0("fc",1:ens_size)])
+  fcs = as.matrix(dt_temp[,.SD,.SDcols = paste0("fc",1:ens_size)])
   
   num_row = nrow(fcs)
   
@@ -79,7 +112,19 @@ forecast_ECC = function(dt = NULL,
   ecc_fcs = data.table(ecc_fcs)
   setnames(ecc_fcs,paste0("ecc_fc",1:ens_size))
   
-  dt = data.table(dt,ecc_fcs)
+  dt_temp = data.table(dt_temp,ecc_fcs)
+  
+  # put back missing locations
+
+  if(!identical(na_rows,integer(0)))
+  {
+    key_dt = key(dt)
+    dt = rbindlist(list(dt_temp,dt[na_rows,]),fill = TRUE)
+    setkeyv(dt,key_dt)
+  } else {
+    dt = dt_temp
+  }
+  
   
   if(saveorgo)
   {
@@ -87,7 +132,6 @@ forecast_ECC = function(dt = NULL,
   }
   
   return(dt)
-
 }
 
 
