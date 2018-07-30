78966a4e92477186b063095ca6d04cab0b4b6b45
Claudio Heinrich <claudio@nr.no>
2018-07-30 10:51:50 +0200

    trying weighted PCA

--- R/auxiliary.functions.R
+++ R/auxiliary.functions.R
@@ -1,18 +1,4 @@
 
-#' truncation function for freezing level of sea water
-#' 
-#' @author Claudio Heinrich
-#' 
-#' @example trc(c(-2,3))
-#' 
-#' @export
-
-trc = function (x){ 
-  truncation.value = -1.769995
-  x = truncation.value * (x < truncation.value) + x * (x >= truncation.value)
-  return(x)}
-
-
 #' crps score for normal distribution, resistant to missing values and 0 standard deviation  
 #'
 #' @param y vector of observations.
@@ -42,35 +28,19 @@ crps.na.rm = function(y, mean, sd){
   return(x)
 }
 
-#' Dawid-Sebastiani score for normal distribution, resistant to missing values and 0 standard deviation  
-#'
-#' @param y vector of observations.
-#' @param mean,sd mean and sd of the forecasting distribution
-#'                   
-#' @return vector of the same length as y containing crps scores.
-#'
-#' @author Claudio Heinrich
-#' @examples crps.na.rm(c(NA,rnorm(10)), 1,1)
-#' 
-#' @importFrom scoringRules crps
-#' 
-#' @export
 
-
-dss = function(y, mean, sd){
-  
-  na_loc = which( is.na(y) | is.na(mean) | is.na(sd) | sd == 0)
-  
-  if(identical(na_loc,integer(0))){
-    x = scoringRules::crps(y, family = "normal", mean = mean, sd = sd)
-  }else{
-    x = rep(0,length(y))
-    x[-na_loc] = scoringRules::crps(y[-na_loc], family = "normal", mean = mean[-na_loc], sd = sd[-na_loc])
-    x[na_loc] = NA
+GneitingWeightFct = function(x,L)
+  {
+    
+    ret_value = rep(0,length(x))
+    
+    ret_value[x == 0] = 1
+    
+    t = x[x != 0]/L
+    ret_value[x != 0] = (1-t)*sin(2*pi*t)/(2*pi*t)+(1-cos(2*pi*t))/(2*pi^2*t)   
+    
+    return(ret_value)
   }
-  return(x)
-}
-
 
 
 
@@ -138,5 +108,19 @@ rhist_dt <- function(B, ens_size = 9, breaks = ens_size + 1, hist_xlab="", hist_
 }
 
 
+
+
+#' truncation function for freezing level of sea water
+#' 
+#' @author Claudio Heinrich
+#' 
+#' @example trc(c(-2,3))
+#' 
+#' @export
+
+trc = function (x){ 
+  truncation.value = -1.769995
+  x = truncation.value * (x < truncation.value) + x * (x >= truncation.value)
+  return(x)}
 
 
