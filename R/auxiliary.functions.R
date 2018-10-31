56d6ffce1454519ea1547db85943ff32bfecda6f
Alex Lenkoski <alex@nr.no>
2018-10-31 12:03:02 +0100

    Permutation test code (#9)

--- R/auxiliary.functions.R
+++ R/auxiliary.functions.R
@@ -63,3 +63,34 @@ trc = function (x,truncation.value = -1.79){
   return(x)}
 
 
+
+#' Run a permutation test of the pairwise difference between two vectors of numbers
+#' @param a Vector, the scores from one method
+#' @param b Vector, the scores from some other method
+#' @param N Integer, the size of the permutation distribution
+#' @return A list with the mean of the difference and the permutation distribution of that difference
+#' @examples
+#' N = 1e2
+#' trend  = 1:N
+#' a = trend + .01 + rnorm(N, .001)
+#' b = trend - .01 + rnorm(N, .001)
+#' l = permutation_test_difference(a,b)
+#' q = sum(l$D <= l$d_bar) / length(l$D)
+#' @author Alex
+#' 
+permutation_test_difference = function(a,
+                                       b,
+                                       N = 5e3){
+    n = length(a)
+    d = a - b
+    d_bar = mean(d)
+    D = NULL
+    for(i in 1:N){
+        swap = rbinom(n,1,0.5)
+        w_swap = which(swap == 1)
+        d_i = d
+        d_i[w_swap] = -d_i[w_swap]
+        D[i] = mean(d_i)
+    }
+    return(list(d_bar = d_bar, D = D))
+}
