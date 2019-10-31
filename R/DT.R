5421e9eca2e0436114dd447da914d6000548583c
Claudio Heinrich <claudio@nr.no>
2019-10-31 11:06:25 +0100

    data documentation

--- R/DT.R
+++ R/DT.R
@@ -1,15 +1,14 @@
 
-
-#' SST forecasts and observations
-#' 
-#' data example of sea surface temperature forecasts issued by the Norwegian climate prediction model for January and February, from 1985 to 2016, 
-#' for the region longitude in (-90,40), latitude in (0,65). The columns Ens1,...,Ens9 are the (raw, i.e. unprocessed) ensemble members of the NorCPM ensemble forecast.
-#' Ens_bar and Ens_sd contain mean and standard deviation of the ensemble forecast. The column SST_bar contains the observation, see section 2.1 in the paper for
-#' details. 
-#' 
-#' @docType data
-#' 
-#' @format A data table.
-#' 
-#' @usage data(DT)
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
 "DT"
